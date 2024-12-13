f2c125d9f58b627992e4b96b72b0afb67d650dc7
paradoxe-tech <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-13 15:23:46 +0100

    added Roxygen comments to signatures

--- R/Loggerhead.r
+++ R/Loggerhead.r
@@ -1,53 +1,46 @@
 library(R6)
 library(TurtleGraphics)
-library(jsonlite)
 
-Loggerhead <- R6Class("Loggerhead",
+Loggerhead <- R6Class("Loggerhead", # nolint: object_name_linter.
   class = TRUE,
- 
+
   public = list(
     layers = NULL,
     activeLayer = NULL,
-    delta = NULL,
-    flow_rate = NULL,
-    printer = NULL,
-    printer_data = NULL,
-    fil_radius = NULL,
-    # Valeurs "génériques" (si l'imprimante n'existe pas dans printers.json)
-    nozzle_diam = 0.2,
-    print_speed = 300,
-    travel_speed = 9000,
+    printerConfig = NULL,
+    userConfig = NULL,
 
-    initialize = function(printer, flow_rate = 1, delta = 0.2) {
+    #' Instancie un objet `Loggerhead`.
+    #' @param printerConfig la configuration de l'imprimante.
+    #' @param userConfig la configuration de l'utilisateur.
+    #' @return Un objet `Loggerhead`.
+    #' @examples
+    #' turtle <- Loggerhead$new(printerConfig, userConfig)
+    #' @export
+    initialize = function(printerConfig, userConfig) {
       self$layers <- list()
-      
-      self$printer <- printer
-      self$printer_data = read_json(paste0(cheminRelatif, "printers.json"))[[printer]]
-      if (is.null(self$printer_data)) {
-        print("WARNING : Data needed to generate GCode start/end sequences for your printer does not exist (check printers.json)")
-      } else {
-        self$nozzle_diam = 0.1
-        self$print_speed = as.numeric(self$printer_data$print_speed)
-        self$travel_speed = as.numeric(self$printer_data$travel_speed)
-        self$fil_radius = as.numeric(self$printer_data$fil_radius)
-      }
-      
-      self$flow_rate <- flow_rate
-      self$delta <- delta
-      
-      turtle_init() # Initialise turtle graphics
+      self$printerConfig <- printerConfig
+      self$userConfig <- userConfig
+      turtle_init()
       turtle_hide()
     },
 
-    # Méthode publique pour ajouter un nouveau calque
+    #' Ajoute un calque à l'objet `Loggerhead`.
+    #' @examples
+    #' turtle <- Loggerhead$new(printerConfig, userConfig)
+    #' turtle$addLayer()
+    #' @export
     addLayer = function() {
       layer <- Path$new()
       self$layers <- append(self$layers, list(layer))
-      layerIndex <- length(self$layers)
-      self$selectLayer(layerIndex)
+      self$selectLayer(length(self$layers))
     },
 
-    # Méthode publique pour changer le calque actif
+    #' Sélectionne un calque de l'objet `Loggerhead`.
+    #' @examples
+    #' turtle <- Loggerhead$new(printerConfig, userConfig)
+    #' turtle$selectLayer(2)
+    #' @export
     selectLayer = function(layerIndex) {
       if (layerIndex < 1 || layerIndex > length(self$layers)) {
         stop("Layer index out of bounds")
@@ -56,12 +49,16 @@ Loggerhead <- R6Class("Loggerhead",
       }
     },
 
-    # Méthode publique pour ajouter des polygones au calque
+    #' Ajoute des formes au calque actif de l'objet `Loggerhead`.
+    #' @examples
+    #' turtle <- Loggerhead$new(printerConfig, userConfig)
+    #' turtle$buildShapes(c(square, rectangle))
+    #' @export
     buildShapes = function(polygons) {
       if(is.null(self$activeLayer)) stop("No active layer selected")
-      
+
       layer <- self$layers[[self$activeLayer]]
-      
+
       for (polygon in polygons) {
         if (!inherits(polygon, "Polygon")) {
           stop("All shapes must be of class 'Polygon'")
@@ -70,76 +67,37 @@ Loggerhead <- R6Class("Loggerhead",
           layer <- Path$new()$fusion(layer, polygonPath)
         }
       }
-      
+
       self$layers[[self$activeLayer]] <- layer
     },
 
-    # Méthode publique pour tracer librement
+    #' Dessine un chemin libre sur le calque actif de l'objet `Loggerhead`.
+    #' @examples
+    #' turtle <- Loggerhead$new(printerConfig, userConfig)
+    #' turtle$freeDraw(c(square, rectangle))
+    #' @export
     freeDraw = function(moves) {
       if(!self$activeLayer) stop("No layer registered")
       # free draw
       self$layers[[self$activeLayer]] <- moves
     },
-    
-    # Méthode publique
-    # Affiche la couche actuelle
-    # (Appelle juste display() de la couche)
+
+    #' Affiche le calque actif de l'objet `Loggerhead`.
+    #' @examples
+    #' turtle <- Loggerhead$new(printerConfig, userConfig)
+    #' turtle$display()
+    #' @export
     display = function() {
       self$layers[[self$activeLayer]]$display()
     },
-    
-    # Méthode publique
-    # Pour générer le GCode correspondant à toutes les couches
-    genFile = function(filename = paste0(cheminRelatif, "out.gcode")) {
-      # En-tête spécifique à l'imprimante
-      gcode_str <- paste("; START SEQUENCE (in printers.json)\n", self$printer_data$start_code, sep='')
 
-      for (i in 1:length(self$layers)) {
-        # Coordonnée Z absolue (i-1 fois la hauteur d'une couche, 
-        # la première couche étant à Z = 0)
-        z <- i*self$delta
-        gcode_str <- paste(gcode_str, sprintf("\n; LAYER %d\n", i), sep = '')
-  
-        movs <- self$layers[[i]]$movements
-        
-        if (length(movs) > 0) {
-          curr_p <- movs[[1]] 
-          # Aller vers le premier point de la couche !
-          # J'ignore la possiblilité où le premier point de la couche aurait FILL
-          # car ça n'a aucune utilité d'extruder en montant simplement sur Z (je pense ?)
-          curr_str <- sprintf("G0 X%f Y%f Z%f F%f E0.000\n", curr_p[1], curr_p[2], z, self$travel_speed/6)
-          gcode_str <- paste(gcode_str, curr_str, sep='')
-          
-          for (j in 1:(length(movs) - 1)) {
-            next_p <- movs[[j+1]]
-            # Mention FILL
-            if (next_p[3] != 0) {
-              ## Toute cette partie sert à calculer le taux d'extrusion pour un vecteur donné
-              # Première étape : savoir la longueur de la ligne qu'on trace
-              curr_line_l <- sqrt((next_p[1] - curr_p[1])**2 + (next_p[2] - curr_p[2])**2)
-              #cat('CURR P\n');print(curr_p); cat('NEXT P\n');print(next_p)
-              # Volume de fil à extruder 
-              extr_v <- self$delta * self$flow_rate * self$nozzle_diam * curr_line_l
-              # On divise par la surface du filament (qui dépend de son rayon)
-              # et on obtient la longueur
-              extr_l <- extr_v / (pi * (self$fil_radius)**2)
-              # Commande G1 : Mouvement de travail (avec extrusion)
-              curr_str <- sprintf("G1 X%f Y%f Z%f F%f E%f\n", next_p[1], next_p[2], z, self$print_speed, extr_l)
-            } else {
-              # Si FILL = 0 : mouvement rapide G0 (mouvement rapide sans extrusion)
-              curr_str <- sprintf("G0 X%f Y%f Z%f F%f E0.000\n", next_p[1], next_p[2], z, self$travel_speed)
-            }
-            gcode_str <- paste(gcode_str, curr_str, sep='')
-            curr_p <- next_p
-          }
-          gcode_str <- paste(gcode_str, "\n")
-        }
-      }
-      # Code de fin spécifique à l'imprimante
-      gcode_str <- paste(gcode_str, "\n\n; END SEQUENCE\n", sep='')
-      gcode_str <- paste(gcode_str, self$printer_data$end_code, sep='')
-      write(gcode_str, filename)
+    #' Exporte l'objet `Loggerhead` au format GCODE.
+    #' @examples
+    #' turtle <- Loggerhead$new(printerConfig, userConfig)
+    #' turtle$export("out.gcode")
+    #' @export
+    export = function(path) {
+      return()
     }
-    
   )
 )
--- R/Path.r
+++ R/Path.r
@@ -9,6 +9,14 @@ Path <- R6Class("Path",
     facing = c(1, 0), # Direction initiale (vecteur x, y)
     movements = list(), # Liste pour stocker les mouvements
 
+    #' Instancie un objet `Path`.
+    #' @param x la coordonnée x de départ.
+    #' @param y la coordonnée y de départ.
+    #' @param facing la direction initiale.
+    #' @return Un objet `Path`.
+    #' @examples
+    #' path <- Path$new(0, 0, c(1, 0))
+    #' @export
     initialize = function(x=0, y=0, facing=c(1, 0)) {
       self$facing <- facing
       self$x <- x
@@ -16,7 +24,12 @@ Path <- R6Class("Path",
       self$movements <- list(c(x, y, 0.0)) # Pas de FILL pour le début du Path
     }, 
 
-    # Constructeur alternatif à partir de coordonnées
+    #' Crée un objet `Path` à partir de coordonnées.
+    #' @param coords les coordonnées du chemin.
+    #' @return Un objet `Path`.
+    #' @examples
+    #' path <- Path$fromCoords(list(c(0, 0), c(1, 1), c(2, 2)))
+    #' @export
     fromCoords = function(coords) {
       path <- Path$new(coords[[1]][1], coords[[1]][2])
 
@@ -29,7 +42,13 @@ Path <- R6Class("Path",
       return(path)
     },
 
-    # Méthode publique pour avancer dans la direction actuelle
+    #' Avance la tortue d'une certaine distance.
+    #' @param value la distance à parcourir.
+    #' @param fill le remplissage du chemin.
+    #' @examples
+    #' path <- Path$new(0, 0, c(1, 0))
+    #' path$forward(10)
+    #' @export
     forward = function(value, fill=1.0) {
       new_x <- self$x + value * self$facing[1]
       new_y <- self$y + value * self$facing[2]
@@ -40,20 +59,38 @@ Path <- R6Class("Path",
       self$y <- new_y
     },
 
-    # Méthode publique pour faire tourner la direction actuelle d'un certain angle
+    #' Tourne la tortue d'un certain angle.
+    #' @param angle l'angle de rotation (en degrés).
+    #' @examples
+    #' path <- Path$new(0, 0, c(1, 0))
+    #' path$turn(90)
+    #' @export
     turn = function(angle) {
       radians <- angle * pi / 180
       new_facing_x <- cos(radians) * self$facing[1] - sin(radians) * self$facing[2]
       new_facing_y <- sin(radians) * self$facing[1] + cos(radians) * self$facing[2]
       self$facing <- round(c(new_facing_x, new_facing_y), 10)
     },
-    
-    # Méthode publique pour modifier directement le vecteur direction actuel
-    set_facing = function(new_facing_x, new_facing_y) {
-      self$facing <- round(c(new_facing_x, new_facing_y), 10)
+
+    #' Définit la direction de la tortue.
+    #' @param direction la direction de la tortue.
+    #' @examples
+    #' path <- Path$new(0, 0, c(1, 0))
+    #' path$setFacing(c(0, 1))
+    #' @export
+    setFacing = function(direction) {
+      self$facing <- round(direction, 10)
     },
 
-    # Fonction publique pour fusionner deux tracés
+    #' Fusionne deux objets `Path`.
+    #' @param path1 le premier chemin.
+    #' @param path2 le deuxième chemin.
+    #' @return Un objet `Path`.
+    #' @examples
+    #' path1 <- Path$new(0, 0, c(1, 0))
+    #' path2 <- Path$new(1, 1, c(1, 0))
+    #' path1$fusion(path2)
+    #' @export
     fusion = function(path1, path2) {
       if (!all(inherits(path1, "Path") && inherits(path2, "Path"))) {
         stop("ERROR : All objects to be merged should be instances of 'Path'")
@@ -75,16 +112,28 @@ Path <- R6Class("Path",
       return(new_path)
     },
 
-    # Méthode publique pour enregistrer un mouvement
-    move = function(x2, y2, fill=1.0) {
-      if (x2 < 0) x2 = 0 # Cropping
-      if (y2 < 0) y2 = 0
+    #' Enregistre un mouvement de la tortue.
+    #' @param x2 la coordonnée x de destination.
+    #' @param y2 la coordonnée y de destination.
+    #' @param fill le remplissage du chemin.
+    #' @examples
+    #' path <- Path$new(0, 0, c(1, 0))
+    #' path$move(1, 1)
+    #' @export
+    move = function(x2, y2, fill = 1.0) {
+      if (x2 < 0) x2 <- 0 # Cropping
+      if (y2 < 0) y2 <- 0
       self$movements <- append(self$movements, list(c(x2, y2, fill)))
       self$x <- x2
       self$y <- y2
     },
 
-    # Surcharge de la fonction d'affichage
+    #' Affiche le chemin sous forme de chaîne de caractères.
+    #' @examples
+    #' path <- Path$new(0, 0, c(1, 0))
+    #' path$move(1, 1)
+    #' path$print()
+    #' @export
     print = function() {
       movement_strings <- sapply(self$movements, function(move) {
         paste0("(", move[1], ", ", move[2], ", ", move[3], ")")
@@ -94,13 +143,17 @@ Path <- R6Class("Path",
       cat("<Path>", movement_path, "\n", sep = " ")
     },
     
-    
-    # Méthode publique pour visualiser un chemin
+    #' Affiche le chemin sous forme de graphique.
+    #' @examples
+    #' path <- Path$new(0, 0, c(1, 0))
+    #' path$move(1, 1)
+    #' path$display()
+    #' @export
     display = function() {
       turtle_init()
-      #turtle_hide()
       turtle_up()
-      
+      turtle_hide()
+
       curr_pos <- self$movements[[1]]
       turtle_goto(curr_pos[1], curr_pos[2])
       for (i in 1:(length(self$movements) - 1)) {
@@ -118,7 +171,7 @@ Path <- R6Class("Path",
           turtle_goto(next_pos[1], next_pos[2])
         }
         Sys.sleep(0.3)
-        pos <- next_pos 
+        pos <- next_pos
       }
     }
   )
